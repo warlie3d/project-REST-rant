747771e25dac841fb0d473761089251aafc5376b
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-10-23 18:32:41 -0400

    adding some notes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//controller
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+//incase you put another route it gives a 404
 app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
 });
