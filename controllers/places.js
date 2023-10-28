a753066ebd36bbd54559e958d044e293e20aea12
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-10-28 13:41:59 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,35 +1,48 @@
 const router = require("express").Router();
-let places = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "http://placekitten.com/250/250",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "http://placekitten.com/250/250",
-  },
-];
+const places = require("..models/places.js");
+
+router.get("/", (req, res) => {
+  res.render("places/Index", { places });
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  let place = req.body;
+  if (!req.body.pic) {
+    place.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    place.city = "Anytown";
+  }
+  if (!req.body.state) {
+    place.state = "USA";
+  }
+  places.push(place);
+  //should give path
+  res.redirect("/places");
+});
+
 router.get("/new", (req, res) => {
   res.render("places/New");
 });
 
+router.delete("/:id", (req, res) => {
+  let index = req.params.id;
+  places.splice(index, 1);
+  res.status(303).redirect("/places");
+  //res.send("Delete a particular place");
+});
+
 router.get("/:id", (req, res) => {
   let place = places[req.params.id];
-  res.render("places/Show", { place });
+  res.render("places/Show", {
+    place: place,
+    index: req.params.id,
+  });
 });
 
 router.put("/:id", (req, res) => {
   res.send("Update a particular place");
-});
-
-router.delete("/:id", (req, res) => {
-  res.send("Delete a particular place");
 });
 
 router.get("/:id/edit", (req, res) => {
@@ -38,20 +51,12 @@ router.get("/:id/edit", (req, res) => {
 });
 
 router.post("/:id/rant", (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
   res.send("Create a rant/comment about a particular place");
 });
 
 router.delete("/:id/rant", (req, res) => {
   res.send("Delete a rant/comment about a particular place");
-});
-
-router.get("/", (req, res) => {
-  res.render("places/Index", { places });
-});
-
-router.post("/", (req, res) => {
-  res.send("Create new place");
 });
 
 module.exports = router;
