034794384daecf4c8cb7f9082d67ae9382b26f31
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-10-28 14:55:48 -0400

    added some stuff

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,11 +8,15 @@ function Show({ place }) {
                 <div className='Details-container'>
                     <h2>{place.name}, {place.city}</h2>
                     <img src={place.pic} alt={place.name} />
+
                     <p>Cuisines: {place.cuisines}</p>
                 </div>
                 <div className='button-container'>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <a href={`/places/${index}/edit`}><button className='btn btn-primary'>Edit</button></a>
+                    <form action={`/places/${index}?_method=DELETE`} method='POST'>
+                        <button className='btn btn-danger'>Delte</button>
+                    </form>
+                    <a href='/places'><button className='btn btn-secondary'>Go to Places</button></a>
                 </div>
             </main>
         </Default>
