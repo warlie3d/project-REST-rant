b0dc24910583225590bda1fa728eac6e1ba01584
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-10-30 21:34:38 -0400

    added some stuff

--- models/places.js
+++ models/places.js
@@ -4,13 +4,13 @@ module.exports = [
     city: "Seattle",
     state: "WA",
     cuisines: "Thai, Pan-Asian",
-    pic: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dcat&psig=AOvVaw3hx885Va8peWYDwkTrVK0c&ust=1698792714755000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIjok5TunoIDFQAAAAAdAAAAABAE",
+    pic: "http://placekitten.com/550/550",
   },
   {
     name: "Coding Cat Cafe",
     city: "Phoenix",
     state: "AZ",
     cuisines: "Coffee, Bakery",
-    pic: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dcat&psig=AOvVaw3hx885Va8peWYDwkTrVK0c&ust=1698792714755000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIjok5TunoIDFQAAAAAdAAAAABAE",
+    pic: "http://placekitten.com/550/550",
   },
 ];
